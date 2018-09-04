2354867bb48dc8fae5277de2239dc560e3414c76
jgutwirth <gutwirth@chapman.edu>
2018-09-04 19:24:01 +0000

    Second commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+/**************************
+
+	Assignment 0: Hello World
+
+	Usage: Print out "Hello World!"
+
+	**************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Hello World!"<<endl;
+	return 0;
+}
